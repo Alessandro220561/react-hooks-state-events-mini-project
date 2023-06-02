56d23c0c9fe9d1060c41d9284e7ea74f92e7ad39
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-06-02 14:06:17 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  
+  function handleCategoryClick(category) {
+    setSelectedCategory(category);
+    //console.log("clicked category:", category)
+  }
+
+  function handleDeleteTask(text) {
+    const updatedTasks = tasks.filter((task) => task.text !== text);
+    setTasks(updatedTasks)
+  }
+
+  const filteredTasks = tasks.filter(task => task.category === selectedCategory || selectedCategory === "All")
+
+  function onTaskFormSubmit(newTask) {
+    //console.log(newTask)
+    setTasks([...tasks, newTask])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={ CATEGORIES }  selectedCategory={ selectedCategory } handleCategoryClick={ handleCategoryClick }/>
+      <NewTaskForm categories={ CATEGORIES.filter((category) => category !== "All") } onTaskFormSubmit={ onTaskFormSubmit }/>
+      <TaskList tasks={ filteredTasks } handleDeleteTask={ handleDeleteTask }/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function Task() {
+function Task({ task, text, category, handleDeleteTask }) {
+  
+  function handleDelete() {
+    handleDeleteTask(text)
+  }
+
+
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{ category }</div>
+      <div className="text">{ text }</div>
+      <button className="delete"  onClick={ handleDelete}  >X</button>
     </div>
   );
 }
